5eca277030ffa2a25dfd63153384a87e5970a4a3
Edwin Lienard <lienard.edwin@gmail.com>
2021-11-06 14:40:17 +0100

    ADD input + FUNCTION find movie by name

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,65 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Layout from "./components/Layout/Layout";
 import { getFilmsFromApiWithSearchedText, getFilmsByPopularity } from './API/TMDBApi'
+import styled from 'styled-components'
 import Poster from "./components/Poster/Poster"
-
 
 type Movies = {
   poster_path: string
   title: string
 }
 
+const TextInput = styled.input `
+    width: 50%;
+  `
+
+const FlexContainer = styled.div `
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+  `
+
 function App() {
-  const [movies, setMovies]= useState<Movies[]>([])
+  const [movies, setMovies] = useState<Movies[]>([])
+  const [textInput, setTextInput] = useState('')
 
   useEffect(() => {
     loadFilms()
   }, [])
 
+  useEffect(() =>{
+    if(textInput.length === 0){
+      loadFilms()
+    }else{
+      loadFilmByText()
+    }
+  },[textInput])
+
   function loadFilms() {
     getFilmsByPopularity().then(data => setMovies(data.results));
+  }
+
+  function handleTextInput(e: React.ChangeEvent<HTMLInputElement>){
+    setTextInput(e.target.value)
+  }
+
+  function loadFilmByText(){
+    if (textInput.length > 0) {
+      getFilmsFromApiWithSearchedText(textInput).then(data => setMovies(data.results))
+    }
   }
 
   return (
     <div className="App">
       <Layout/>
-      {
-        movies.map((res:Movies) =>(
-          <Poster data={res}/>
-        ))
-      }
+      <TextInput onChange={handleTextInput} value={textInput} placeholder={'Chercher ici'}/>
+      <FlexContainer>
+        {
+          movies.map((res:Movies) =>(
+            <Poster data={res}/>
+          ))
+        }
+      </FlexContainer>
     </div>
   );
 }
